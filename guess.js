ce22709dccf4dbdf0ac45285f60bb9b95d3e3b7a
bhavik712 <baradbhavik41@gmail.com>
2023-07-10 18:37:11 +0530

    Guess game

---
+++ guess.js
@@ -0,0 +1,21 @@
+//here we genrate a num using random function between 1-5 and guessing by using passing our value to function
+//if our guess is true it shows won else false  
+let min =1
+let max =5
+
+let number = Math.floor(Math.random() * (max- min +1)) + min
+
+let guessgame = function(num){
+    if(num == number){
+        console.log('Won')
+    }
+    else{
+        console.log('Lost')
+    }
+}
+
+guessgame(2)
+guessgame(3)
+guessgame(1)
+guessgame(5)
+guessgame(5)
