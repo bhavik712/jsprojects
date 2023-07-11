f26cd4d5744bd5b287e2aba20062227e1d29e287
bhavik712 <baradbhavik41@gmail.com>
2023-07-11 11:16:04 +0530

    cammel case

--- meathods.js
+++ meathods.js
@@ -1,32 +1,32 @@
 //use of meathods to changing the properties of object
 let resturant = {
     name: 'Govinda',
-    total_capicity : 60,
-    guest_count: 0,
-    checkAvailability: function (newguest){
-        let seats_left = this.total_capicity - this.guest_count
-        return newguest <= seats_left
+    totalCapicity : 60,
+    guestCount: 0,
+    checkAvailability: function (newGuest){
+        let seatsLeft = this.totalCapicity - this.guestCount
+        return newGuest <= seatsLeft
     },
-    addguestcount: function(newguest){
-        if(resturant.checkAvailability(newguest) == true){
-            this.guest_count = this.guest_count + newguest
-            console.log(`new ${newguest} added. total ${this.guest_count} guests`)
+    addGuestCount: function(newGuest){
+        if(resturant.checkAvailability(newGuest) == true){
+            this.guestCount = this.guestCount + newGuest
+            console.log(`new ${newGuest} added. total ${this.guestCount} guests`)
         }
         else{
             console.log('Sorry, No place Available')
         }
 
     },
-    removecount: function(goodbyeguest){
-        this.guest_count = this.guest_count - goodbyeguest
-        console.log(`${goodbyeguest} went. total ${this.guest_count} guests in restaurent`)
+    removeCount: function(goodByeGuest){
+        this.guestCount = this.guestCount - goodByeGuest
+        console.log(`${goodByeGuest} went. total ${this.guestCount} guests in restaurent`)
 
     }
 
 }
 
 
-resturant.addguestcount(15)
-resturant.addguestcount(10)
-resturant.removecount(7)
+resturant.addGuestCount(15)
+resturant.addGuestCount(10)
+resturant.removeCount(7)
 
